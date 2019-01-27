9f552b014228d8c3efa08b14b0dea638c11030ab
D.Biles <darron.biles@gmail.com>
2019-01-27 03:06:01 -0500

    [FIX] - to update server

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
       <nav>
         <div className="nav-wrapper grey darken-3">
           <a href="#!" className="brand-logo center">Digital Billboards - Control Panel</a>
-          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
           <ul className="right hide-on-med-and-down">
             <li><a href="/promotions/">Promotions</a></li>
             <li><a href="/users/">Users</a></li>
