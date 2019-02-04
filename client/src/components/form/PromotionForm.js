fd51510337ea0a7b303f6b1742f6a60e32eb1685
Mike Gross <grossmi4@gmail.com>
2019-02-04 13:33:27 -0500

    added dropdown for beacon tag

--- client/src/components/form/PromotionForm.js
+++ client/src/components/form/PromotionForm.js
@@ -1,42 +1,58 @@
 import React from 'react';
 import TextInput from "./TextInput";
-import DatePickerInput from "./DatePickerInput";
 import { Input } from 'react-materialize';
 import './form.css';
-import $ from 'jquery';
 
 const PromotionForm = props => {
   return (
     <form onSubmit={props.handleSubmit}>
       <div className="row top-panel">
         <div className="col s12 z-depth-1 form-card">
-          <h5>New Promotion</h5>
-          <TextInput
-            name="PromotionText"
-            label="Promotional Text"
-            value={props.PromotionText}
-            onChange={props.handleInputChange}
-          />
-          <TextInput
-            name="BeaconTag"
-            label="Beacon Tag"
-            value={props.BeaconTag}
-            onChange={props.handleInputChange}
-          />
-          <TextInput
-            name="PreferenceGroup"
-            label="Preference Group"
-            value={props.PreferenceGroup}
-            onChange={props.handleInputChange}
-          />
+          <h5>Create a New Promotion</h5>
           <div className="col s12">
-            <Input
-              name="ExpirationDate"
-              label="Expiration Date"
-              value={props.ExpirationDate}
-              type="date"
+            <TextInput
+              name="PromotionText"
+              label="Promotional Text"
+              value={props.PromotionText}
               onChange={props.handleInputChange}
             />
+          </div>
+          <div className="col s12">
+            <TextInput
+              name="PreferenceGroup"
+              label="Preference Group"
+              value={props.PreferenceGroup}
+              onChange={props.handleInputChange}
+            />
+          </div>
+          <div className="col s12">
+            <div className="col s12">
+              <Input
+                name="BeaconTag"
+                label="Beacon Tag"
+                type="select"
+                s={12}
+                value={props.BeaconTag}
+                onChange={props.handleInputChange}
+              >
+                <option value="mint">Mint</option>
+                <option value="ice">Ice</option>
+                <option value="coconut">Coconut</option>
+                <option value="blueberry">Blueberry</option>
+              </Input>
+            </div>
+          </div>
+          <div className="col s12">
+            <div className="col s12">
+              <Input
+                s={12}
+                name="ExpirationDate"
+                label="Expiration Date"
+                value={props.ExpirationDate}
+                type="date"
+                onChange={props.handleInputChange}
+              />
+            </div>
           </div>
             <div className="col s12 submit-btn-container">
               <button
