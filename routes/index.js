5b2b6326f42712015a8c324582b1a63a0b701371
Mike <grossmi4@gmail.com>
2019-02-05 19:35:04 -0500

    added path

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 router.use("/api", apiRoutes);
 
 
-router.use("/*", function(req, res) {
+router.route.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
